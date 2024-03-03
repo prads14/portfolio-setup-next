00d03ecf9bad3868c3a038824cd3b026688636bd
prads14 <pradnyaprachi@gmail.com>
2024-03-03 17:21:30 +0530

    statice data binding

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,10 @@
 import { Poppins } from "next/font/google"
 import { Oswald } from "next/font/google";
-
+import { Lusitana } from "next/font/google";
 
 export const poppins = Poppins({weight:"400", subsets: ['latin'] });
 
 export const oswald = Oswald({weight: "400", subsets: ['cyrillic']})
+
+export const lusitana = Lusitana({ weight: "400", subsets:['latin']})
 
