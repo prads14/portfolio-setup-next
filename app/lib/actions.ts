06fe6890c0bdc4b0d530297b93352e8c4c02c023
prads14 <pradnyaprachi@gmail.com>
2024-03-04 12:20:53 +0530

    CRUD operations completed, coupled with SQL commands

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -38,3 +38,39 @@ export async function createInvoice(formData:FormData) {
     redirect('/dashboard/invoices')
     
 }
+
+const UpdateInvoice = FormSchema.omit({id: true, date: true})
+
+export async function updateInvoice(id:string, formData: FormData) {
+    const {customerId, amount, status} = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    })
+
+    const amountInCents = amount * 100;
+
+    await sql`
+        UPDATE invoices
+        SET customer_id=${customerId}, amount=${amount}, status=${status}
+        WHERE id=${id}
+    `
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices')
+}
+
+const DeleteInvoice = FormSchema.omit({id: true, date: true})
+
+
+export async function deleteInvoice(id:string) {
+
+    await sql`
+        DELETE from invoices
+        WHERE id=${id}
+    `;
+
+    revalidatePath('/dashboard/invoices')
+
+    
+}
